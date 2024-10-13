b6672e10709ddfc1c524991af95d1cdd26b70ea3
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-13 02:40:41 -0300

    Error Handling

--- server/controllers/eventos.controllers.js
+++ server/controllers/eventos.controllers.js
@@ -1,55 +1,78 @@
+import { response } from "express";
 import { pool } from "../db.js";
 
 export const getEventos = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM eventos");
-  res.json(result);
+  try {
+    const [result] = await pool.query("SELECT * FROM eventos");
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getEvento = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM eventos WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("SELECT * FROM eventos WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.length === 0)
-    return res.status(404).json({ message: "Evento not found" });
+    if (result.length === 0)
+      return res.status(404).json({ message: "Evento not found" });
 
-  res.json(result[0]);
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createEvento = async (req, res) => {
-  const { fecha_inicio, fecha_fin, lugar, descripcion, tematica } = req.body;
-  const [result] = await pool.query(
-    "INSERT INTO eventos(fecha_inicio, fecha_fin, lugar, descripcion, tematica) VALUES (?, ?, ?, ?, ?)",
-    [fecha_inicio, fecha_fin, lugar, descripcion, tematica]
-  );
-  res.json({
-    id: result.insertId,
-    fecha_inicio,
-    fecha_fin,
-    lugar,
-    descripcion,
-    tematica,
-  });
+  try {
+    const { fecha_inicio, fecha_fin, lugar, descripcion, tematica } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO eventos(fecha_inicio, fecha_fin, lugar, descripcion, tematica) VALUES (?, ?, ?, ?, ?)",
+      [fecha_inicio, fecha_fin, lugar, descripcion, tematica]
+    );
+    res.json({
+      id: result.insertId,
+      fecha_inicio,
+      fecha_fin,
+      lugar,
+      descripcion,
+      tematica,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deleteEvento = async (req, res) => {
-  const [result] = await pool.query("DELETE FROM eventos WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("DELETE FROM eventos WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.affectedRows === 0)
-    return res.status(404).json({ message: "Evento not found" });
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Evento not found" });
 
-  return res.sendStatus(204);
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateEvento = async (req, res) => {
-  const [result] = await pool.query("UPDATE eventos SET ? WHERE id = ?", [
-    req.body,
-    req.params.id,
-  ]);
-  if (result.affectedRows === 0)
-    return res.status(404).json({ message: "Evento not found" });
-  const [updEvento] = await pool.query("SELECT * FROM eventos WHERE id = ?", [req.params.id])
-  res.json(updEvento);
+  try {
+    const [result] = await pool.query("UPDATE eventos SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Evento not found" });
+    const [updEvento] = await pool.query("SELECT * FROM eventos WHERE id = ?", [
+      req.params.id,
+    ]);
+    res.json(updEvento);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
