368a16f369cd15f8b850cf629528a78a052ea56f
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-16 15:12:16 -0300

    Cambios minimos

--- database/db.sql
+++ database/db.sql
@@ -7,3 +7,33 @@ CREATE TABLE eventos (
     descripcion VARCHAR(300),
     tematica VARCHAR(100)
 );
+
+-- Tabla escultores
+CREATE TABLE escultores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    nacionalidad VARCHAR(100),
+    fecha_nacimiento DATE,
+    edad INT,
+    biografia VARCHAR(300),
+    email VARCHAR(150),
+    telefono VARCHAR(20),
+    foto_perfil BLOB
+);
+
+-- Tabla obras
+CREATE TABLE obras (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_creacion DATE,
+    descripcion TEXT,
+    material VARCHAR(100),
+    estilo VARCHAR(100),
+    calificacion DECIMAL(3, 2)
+);
+
+-- Tabla vota
+CREATE TABLE vota (
+    
+);
+
