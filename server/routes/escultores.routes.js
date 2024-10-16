fcfe19dea365e15c1331e405567c656126e051a7
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-16 18:04:08 -0300

    CRUD Escultores Backend DONE

--- server/routes/escultores.routes.js
+++ server/routes/escultores.routes.js
@@ -3,18 +3,20 @@ import {
   getEscultores,
   getEscultor,
   createEscultor,
+  updateEscultor,
+  deleteEscultor,
 } from "../controllers/escultores.controllers.js";
 
 const router = Router();
 
 router.get("/escultores", getEscultores);
+
 router.get("/escultores/:id", getEscultor);
 
 router.post("/escultores", createEscultor);
-/* 
 
-router.put("/escultores/:id", updateEvento);
+router.put("/escultores/:id", updateEscultor);
 
-router.delete("/escultores/:id", deleteEvento);
- */
+router.delete("/escultores/:id", deleteEscultor);
+
 export default router;
