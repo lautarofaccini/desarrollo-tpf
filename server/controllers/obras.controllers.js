ad669e47a92c42d4c2e81e019be6d563f745fc13
fanafer <fackundofernandez@gmail.com>
2024-10-16 20:33:36 -0300

    CRUD obras done

--- server/controllers/obras.controllers.js
+++ server/controllers/obras.controllers.js
@@ -25,14 +25,19 @@ export const getObra = async (req, res) => {
 };
 
 export const createObra = async (req, res) => {
-    try {
-      const { fecha_creacion, descripcion, material, estilo, calificacion, id_evento, id_escultor } = req.body;
-      const [result] = await pool.query(
-        "INSERT INTO obras(fecha_creacion, descripcion, material, estilo, calificacion, id_evento, id_escultor) VALUES (?, ?, ?, ?, ?, ?, ?)",
-        [fecha_creacion, descripcion, material, estilo, calificacion, id_evento, id_escultor]
-      );
-      res.json({
-        id_obra: result.insertId,
+  try {
+    const {
+      fecha_creacion,
+      descripcion,
+      material,
+      estilo,
+      calificacion,
+      id_evento,
+      id_escultor,
+    } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO obras(fecha_creacion, descripcion, material, estilo, calificacion, id_evento, id_escultor) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      [
         fecha_creacion,
         descripcion,
         material,
@@ -40,23 +45,52 @@ export const createObra = async (req, res) => {
         calificacion,
         id_evento,
         id_escultor,
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  };
-  
-  export const deleteEvento = async (req, res) => {
-    try {
-      const [result] = await pool.query("DELETE FROM eventos WHERE id_evento = ?", [
-        req.params.id,
-      ]);
-  
-      if (result.affectedRows === 0)
-        return res.status(404).json({ message: "Evento not found" });
-  
-      return res.sendStatus(204);
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  };
+      ]
+    );
+    res.json({
+      id_obra: result.insertId,
+      fecha_creacion,
+      descripcion,
+      material,
+      estilo,
+      calificacion,
+      id_evento,
+      id_escultor,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteObra = async (req, res) => {
+  try {
+    const [result] = await pool.query("DELETE FROM obras WHERE id_obra = ?", [
+      req.params.id,
+    ]);
+
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Obra not found" });
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateObra = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "UPDATE obras SET ? WHERE id_obra = ?",
+      [req.body, req.params.id]
+    );
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Obra not found" });
+    const [updObra] = await pool.query(
+      "SELECT * FROM obras WHERE id_obra = ?",
+      [req.params.id]
+    );
+    res.json(updObra);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import eventosRoutes from "./routes/eventos.routes.js";
 import escultoresRoutes from './routes/escultores.routes.js'
+import obrasRoutes from './routes/obras.routes.js'
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(eventosRoutes);
 app.use(escultoresRoutes);
+app.use(obrasRoutes);
 
 app.listen(PORT);
