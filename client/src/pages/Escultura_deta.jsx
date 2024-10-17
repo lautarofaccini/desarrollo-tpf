5080f7af0223c1160e553fd96cd5d703f7145860
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-17 17:16:48 -0300

    Algunos arreglos para mostrar en clase :)

--- client/src/pages/Escultura_deta.jsx
+++ client/src/pages/Escultura_deta.jsx
@@ -1,5 +1,4 @@
 // src/components/EsculturaDetail.js
-import React from 'react';
 import { useParams } from 'react-router-dom';
 import { esculturas } from './esculturas';
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 function HomePage() {
   return (
-    <div className="text-white text-3xl">HomePage</div>
+    <div className="text-white text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, voluptate incidunt iusto eligendi ipsum dolores accusantium aut ducimus magnam cum voluptatibus sint minus sit, perspiciatis consectetur labore, mollitia dolorum! Eos.
+    Laudantium magni quo at a vero soluta nobis totam architecto cumque cum quasi labore, consequuntur debitis sunt tempore natus quae sequi animi magnam omnis et. Numquam expedita nisi odit eaque!
+    Quis voluptatem possimus, culpa modi quisquam consequuntur nesciunt facere? Numquam obcaecati corrupti quisquam. Fugiat repellat hic at maiores minus, veniam eaque harum labore expedita omnis, delectus nulla laboriosam illum nostrum!
+    A laboriosam possimus incidunt ipsam quas, veritatis est accusamus ea recusandae dolores porro sint voluptatibus corporis? Deserunt a eveniet animi debitis ipsum, dignissimos voluptas voluptates, repudiandae maiores eius recusandae atque!
+    Nam, facere! Molestiae praesentium nemo harum reprehenderit architecto. Consequuntur quia deserunt eligendi adipisci explicabo voluptas modi amet sit iure, vero tenetur praesentium tempora culpa laboriosam perferendis saepe soluta quos? Nobis.</div>
   )
 }
 
