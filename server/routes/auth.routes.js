349ffbd7421f75c84b85d6e221f5c48d2fecfa02
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-17 14:42:33 -0300

    Validación de datos al iniciar sesion/registrarse

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,12 +6,14 @@ import {
   profile,
 } from "../controllers/auth.controllers.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schemas.js";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
 
-router.post("/register", register);
+router.post("/login", validateSchema(loginSchema), login);
 
 router.post("/logout", logout);
 
