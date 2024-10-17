5864c9d6dc4a3d301cb954b5e6488c61ab645557
Mauri <95150798+MauriMS@users.noreply.github.com>
2024-10-17 16:02:17 -0300

    Esculturas: bases hechas

---
+++ client/src/pages/Escultura_deta.jsx
@@ -0,0 +1,26 @@
+// src/components/EsculturaDetail.js
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { esculturas } from './esculturas';
+
+const EsculturaDetail = () => {
+  const { id } = useParams();
+  const escultura = esculturas.find((e) => e.id === parseInt(id));
+
+  if (!escultura) {
+    return <div>Escultura no encontrada</div>;
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex flex-col items-center">
+        <img src={escultura.imagen} alt={escultura.nombre} className="w-full max-w-md object-cover" />
+        <h1 className="text-3xl font-bold mt-4 text-white">{escultura.nombre}</h1>
+        <p className="mt-2 text-gray-700">{escultura.descripcion}</p>
+        {/* aca hay que agregar para que se puedan ver las otras imagenes cuando entra a la escultura*/}
+      </div>
+    </div>
+  );
+};
+
+export default EsculturaDetail;
