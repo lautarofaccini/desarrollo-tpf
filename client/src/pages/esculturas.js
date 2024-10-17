68bdf4ac1b43b67eaf102669f1ef95b5d8b73d55
Mauri <95150798+MauriMS@users.noreply.github.com>
2024-10-17 16:02:17 -0300

    Esculturas: bases hechas

---
+++ client/src/pages/esculturas.js
@@ -0,0 +1,20 @@
+export const esculturas = [
+    {
+        id: 1,
+        nombre: "El muñe",
+        imagen: "../../public/5f24099a-aa8e-43c1-9509-43b0497974bf.png",
+        descripcion: "Esta es una obra inspirada",
+    },
+    {
+        id: 2,
+        nombre: "Genial no?",
+        imagen: "../../public/Captura de pantalla 2024-10-17 121649.png",
+        descripcion: "Curso de genial no?",
+    },
+    {
+        id: 3,
+        nombre: "Titular",
+        imagen: "../../public/a5b31958-c7b0-4bf6-826c-ed8a0be41284.jpg",
+        descripcion: "Hincha de boca",
+    },
+];
