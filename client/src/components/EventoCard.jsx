acb42e7f536ad2bf2bd347c6bd5665c757baaa26
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-20 19:57:25 -0300

    Cambios en la pagina evento, componentes nuevos y pagina de evento N para agregar relacion entre evento y obras / escultores

--- client/src/components/EventoCard.jsx
+++ client/src/components/EventoCard.jsx
@@ -1,14 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import { useEventos } from "../context/EventoContext";
-import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import EventoButtons from "./EventoButtons";
 
 function EventoCard({ evento }) {
-  const { isAuthenticated } = useAuth();
-  const { deleteEvento } = useEventos();
-  const navigate = useNavigate();
   return (
     <div className="bg-zinc-700 text-white rounded-md p-4">
-      <p className="text-xl font-bold">{evento.lugar}</p>
+      <Link to={`/eventos/${evento.id_evento}`} className="text-xl font-bold">
+        {evento.lugar}
+      </Link>
       <p className="text-sm">{evento.descripcion}</p>
       <p className="text-sm">{evento.tematica}</p>
       <p className="text-xs">
@@ -20,25 +18,7 @@ function EventoCard({ evento }) {
           new Date(evento.fecha_fin).toLocaleDateString()
         }
       </p>
-
-      {isAuthenticated && (
-        <>
-          <div className="flex gap-x-2">
-            <button
-              className="bg-red-500 px-2 py-1 text-white rounded-md"
-              onClick={() => deleteEvento(evento.id_evento)}
-            >
-              Eliminar
-            </button>
-            <button
-              className="bg-slate-500 px-2 py-1 text-white rounded-md"
-              onClick={() => navigate("/eventos/edit/" + evento.id_evento)}
-            >
-              Editar
-            </button>
-          </div>
-        </>
-      )}
+      <EventoButtons id_evento={evento.id_evento} />
     </div>
   );
 }
---
+++ client/src/pages/EventoPage.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useEventos } from "../context/EventoContext";
+import { useParams } from "react-router-dom";
+import EventoButtons from "../components/EventoButtons";
+
+function EventoPage() {
+  const [evento, setEvento] = useState();
+  const [loading, setLoading] = useState(true);
+  const { getEvento } = useEventos();
+  const params = useParams();
+
+  useEffect(() => {
+    async function loadEvento() {
+      try {
+        const res = await getEvento(params.id);
+        setEvento(res);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadEvento();
+  }, [getEvento, params.id]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <section className="flex justify-center items-center">
+      <div className="bg-zinc-700 text-white rounded-md p-4">
+        <div className="text-xl font-bold">{evento.lugar}</div>
+        <p className="text-sm">{evento.descripcion}</p>
+        <p className="text-sm">{evento.tematica}</p>
+        <p className="text-xs">
+          {new Date(evento.fecha_inicio).toLocaleDateString()}
+        </p>
+        <p className="text-xs">
+          {
+            //Falta hora
+            new Date(evento.fecha_fin).toLocaleDateString()
+          }
+        </p>
+        <EventoButtons id_evento={evento.id_evento} />
+      </div>
+    </section>
+  );
+}
+
+export default EventoPage;
