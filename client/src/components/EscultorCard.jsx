43e747155f2a9c0f6f5805fab703d5de5fae5e87
Lautaro Faccini <lautarofaccini@gmail.com>
2024-10-30 13:13:33 -0300

    Pagina Escultores / Escultor (Falta form)

---
+++ client/src/components/EscultorCard.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+function EscultorCard({ escultor }) {
+  return (
+    <div
+      key={escultor.id_escultor}
+      className="border rounded-lg overflow-hidden shadow-lg flex flex-col h-full min-h-[400px]"
+    >
+      <img
+        src={escultor.foto_perfil || "/JEFF_KOONS_2.jpg"}
+        alt={escultor.nombre}
+        className="w-full h-64 object-cover"
+      />
+      <div className="p-2 flex-grow">
+        <h2 className="text-xl font-semibold text-white">{escultor.nombre} {escultor.apellido}</h2>
+        <p className="text-gray-400">{escultor.biografia}</p>
+        <p className="text-gray-400">{escultor.edad}</p>
+        <p className="text-gray-400">{escultor.email}</p>
+      </div>
+      <div className="p-2 mt-auto">
+        <Link
+          to={`/escultores/${escultor.id_escultor}`}
+          className="mt-2 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 w-full text-center"
+        >
+          Ver más
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default EscultorCard;
