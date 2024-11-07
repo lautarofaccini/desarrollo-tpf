023cce7eec815880207be89da9758959f7d8a01b
Juani <gez.schegtel_ji@proton.me>
2024-11-07 16:52:46 -0300

    Añadimos controles para las fechas

--- database/esquema_desarrollo_tpf.sql
+++ database/esquema_desarrollo_tpf.sql
@@ -1,73 +1,66 @@
-<<<<<<< HEAD
-=======
 DROP DATABASE IF EXISTS desarrollo_tpf;
->>>>>>> produccion/main
 CREATE DATABASE IF NOT EXISTS desarrollo_tpf;
-
 USE desarrollo_tpf;
 
 -- Tabla de eventos
 CREATE TABLE eventos (
-<<<<<<< HEAD
-	id_evento INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	fecha_inicio DATETIME NOT NULL,
-	fecha_fin DATETIME NOT NULL,
-	lugar VARCHAR(255) NOT NULL,
-	descripcion TEXT,
-	tematica VARCHAR(255)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-=======
     id_evento INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     fecha_inicio DATETIME NOT NULL,
     fecha_fin DATETIME NOT NULL,
     lugar VARCHAR(255) NOT NULL,
     descripcion TEXT,
     tematica VARCHAR(255),
-		CONSTRAINT chk_fecha_valida CHECK (fecha_inicio < fecha_fin)
+		CONSTRAINT chk_fecha_valida CHECK (fecha_inicio <= fecha_fin)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
->>>>>>> produccion/main
 
 -- Tabla escultores
 CREATE TABLE escultores (
-	id_escultor INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(100) NOT NULL,
-	apellido VARCHAR(100) NOT NULL,
-	nacionalidad VARCHAR(100) NOT NULL,
-	fecha_nacimiento DATE NOT NULL,
-	edad INT COMMENT 'Atributo derivado.',
-	biografia TEXT NOT NULL,
-	email VARCHAR(150),
-	telefono VARCHAR(20),
-	foto_perfil BLOB
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+    id_escultor INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    nacionalidad VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    edad INT COMMENT 'Atributo derivado.',
+    biografia TEXT NOT NULL,
+    email VARCHAR(150),
+    telefono VARCHAR(20),
+    foto_perfil BLOB
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla obras
 CREATE TABLE obras (
-	id_obra INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	fecha_creacion DATE,
-	descripcion TEXT,
-	material VARCHAR(100),
-	estilo VARCHAR(100),
-	calificacion DECIMAL(3, 2),
+  id_obra INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  fecha_creacion DATE,
+  descripcion TEXT,
+  material VARCHAR(100),
+  estilo VARCHAR(100),
+  calificacion DECIMAL(3, 2),
 	id_evento INT UNSIGNED,
 	id_escultor INT UNSIGNED NOT NULL,
-	CONSTRAINT fk_obras_eventos FOREIGN KEY (id_evento) REFERENCES eventos(id_evento) ON DELETE
-	SET
-		NULL,
-		CONSTRAINT fk_obras_escultores FOREIGN KEY (id_escultor) REFERENCES escultores(id_escultor)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+	CONSTRAINT fk_obras_eventos FOREIGN KEY (id_evento) REFERENCES eventos(id_evento) ON DELETE SET NULL,
+	CONSTRAINT fk_obras_escultores FOREIGN KEY (id_escultor) REFERENCES escultores(id_escultor)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla imágenes
 CREATE TABLE imagenes (
 	id_imagen INT UNSIGNED AUTO_INCREMENT,
-	url VARCHAR(255) NOT NULL,
+	url VARCHAR(255),
 	id_obra INT UNSIGNED,
 	PRIMARY KEY(id_imagen, id_obra),
 	CONSTRAINT fk_imagenes_obras FOREIGN KEY (id_obra) REFERENCES obras(id_obra) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla usuarios
 CREATE TABLE usuarios (
@@ -76,7 +69,10 @@ CREATE TABLE usuarios (
 	password VARCHAR(255) NOT NULL,
 	nickname VARCHAR(30) NOT NULL UNIQUE,
 	rol ENUM('admin', 'user') NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla vota
 CREATE TABLE vota (
@@ -86,11 +82,11 @@ CREATE TABLE vota (
 	PRIMARY KEY (id_usuario, id_obra),
 	CONSTRAINT fk_vota_usuarios FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
 	CONSTRAINT fk_vota_obras FOREIGN KEY (id_obra) REFERENCES obras(id_obra),
-	CONSTRAINT chk_puntaje CHECK (
-		puntaje BETWEEN 1
-		AND 5
-	)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+	CONSTRAINT chk_puntaje CHECK (puntaje BETWEEN 1 AND 5)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla participa
 CREATE TABLE participa (
@@ -99,36 +95,34 @@ CREATE TABLE participa (
 	PRIMARY KEY (id_escultor, id_evento),
 	CONSTRAINT fk_participa_escultor FOREIGN KEY (id_escultor) REFERENCES escultores(id_escultor),
 	CONSTRAINT fk_participa_evento FOREIGN KEY (id_evento) REFERENCES eventos(id_evento)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
-/* Trigger que calcula la edad de un escultor cada vez que se registre uno en la base de datos. */
-DELIMITER $ $ CREATE TRIGGER calcular_edad_al_insertar BEFORE
-INSERT
-	ON escultores FOR EACH ROW BEGIN
-SET
-	NEW.edad = TIMESTAMPDIFF(YEAR, NEW.fecha_nacimiento, CURDATE());
+/* Trigger que calcula la edad de un escultor cada vez que se registre uno en la base de datos. */ 
+DELIMITER $$
+CREATE TRIGGER calcular_edad_al_insertar
+BEFORE INSERT ON escultores
+FOR EACH ROW
+BEGIN
+	SET NEW.edad = TIMESTAMPDIFF(YEAR, NEW.fecha_nacimiento, CURDATE());
+END $$
+DELIMITER ;
 
-END $ $ DELIMITER;
-
-/* Trigger que calcula la edad de un escultor cada vez que se actualice la fecha de nacimiento de uno en la base de datos. */
-DELIMITER $ $ CREATE TRIGGER calcular_edad_al_actualizar BEFORE
-UPDATE
-	ON escultores FOR EACH ROW BEGIN IF NEW.fecha_nacimiento <> OLD.fecha_nacimiento THEN
-SET
-	NEW.edad = TIMESTAMPDIFF(YEAR, NEW.fecha_nacimiento, CURDATE());
-
-END IF;
-
-END $ $ DELIMITER;
+/* Trigger que calcula la edad de un escultor cada vez que se actualice la fecha de nacimiento de uno en la base de datos. */ 
+DELIMITER $$
+CREATE TRIGGER calcular_edad_al_actualizar
+BEFORE UPDATE ON escultores
+FOR EACH ROW
+BEGIN
+	IF NEW.fecha_nacimiento <> OLD.fecha_nacimiento THEN
+		SET NEW.edad = TIMESTAMPDIFF(YEAR, NEW.fecha_nacimiento, CURDATE());
+	END IF;
+END $$
+DELIMITER ;
 
 /* Evento para calcular la edad. */
-<<<<<<< HEAD
-CREATE EVENT IF NOT EXISTS actualizar_edad_escultores ON SCHEDULE EVERY 1 YEAR STARTS NOW() DO
-UPDATE
-	escultores
-SET
-	edad = TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE());
-=======
 CREATE EVENT IF NOT EXISTS actualizar_edad_escultores
 ON SCHEDULE EVERY 1 YEAR
 STARTS NOW()
@@ -161,7 +155,6 @@ END $$
 DELIMITER ; 
 
 /* Trigger que controla la fecha de inicio y de fin en la actualización. */
-DROP TRIGGER check_fecha_actualizacion;
 DELIMITER $$
 
 CREATE TRIGGER check_fecha_actualizacion
@@ -177,7 +170,7 @@ BEGIN
         WHERE YEAR(fecha_inicio) = YEAR(NEW.fecha_inicio)
         AND id_evento != NEW.id_evento
     ) THEN
-        SET msg = 'El nuevo año de inicio coincide con uno existente.';
+        SET msg = 'El año de inicio debe ser único.';
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
     END IF;
 
@@ -188,26 +181,12 @@ BEGIN
         WHERE YEAR(fecha_fin) = YEAR(NEW.fecha_fin)
         AND id_evento != NEW.id_evento
     ) THEN
-        SET msg = 'El nuevo año de fin coincide con uno existente.';
+        SET msg = 'El año de fin debe ser único.';
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
     END IF;
-    
-     -- Verificar que la nueva fecha de fin no sea anterior a la fecha de inicio
-    IF EXISTS (
-        SELECT 1
-        FROM eventos
-        -- WHERE YEAR(fecha_fin) < YEAR(NEW.fecha_fin)
-        WHERE timestampdiff(day,fecha_fin,NEW.fecha_fin) < 0
-        AND id_evento = NEW.id_evento
-    ) THEN
-        SET msg = 'El nuevo año de fin es anterior a la fecha de inicio.';
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
-    END IF;
+END $$
 
-END $$
-
-DELIMITER ;
+DELIMITER ;
 
 /* Vista para ver las fechas ordenadas por la fecha de inicio de mayor a menor. */ 
 CREATE VIEW vista_fechas AS SELECT fecha_inicio, fecha_fin FROM eventos ORDER BY (fecha_inicio) DESC;
->>>>>>> produccion/main
