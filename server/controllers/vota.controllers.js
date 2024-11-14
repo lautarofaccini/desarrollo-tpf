2536544f01c92426978d56849dd54da776560c69
Lautaro Faccini <lautarofaccini@gmail.com>
2024-11-14 13:23:24 -0300

    Ahora se puede registrar el voto de un usuario

---
+++ server/controllers/vota.controllers.js
@@ -0,0 +1,112 @@
+import { pool } from "../db.js";
+
+// Obtener todos los votos o un voto específico
+export const getVotos = async (req, res) => {
+  try {
+    const { idObra, idUsu } = req.query;
+
+    if (idObra && idUsu) {
+      // Si se proporcionan idObra e idUsu, devolver el voto específico
+      const [result] = await pool.query(
+        "SELECT * FROM vota WHERE id_obra = ? AND id_usuario = ?",
+        [idObra, idUsu]
+      );
+
+      if (result.length === 0)
+        return res.status(404).json({ message: "Voto not found" });
+
+      return res.json(result[0]);
+    }
+
+    // Si no se proporcionan parámetros, devolver todos los votos
+    const [result] = await pool.query("SELECT * FROM vota");
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Obtener votos por obra
+export const getVotosByObra = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM vota WHERE id_obra = ?", [
+      req.params.id_obra,
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Obtener votos por usuario
+export const getVotosByUsuario = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "SELECT * FROM vota WHERE id_usuario = ?",
+      [req.params.id_usuario]
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Crear un voto
+export const createVoto = async (req, res) => {
+  try {
+    const { id_obra, id_usuario, puntaje } = req.body;
+
+    const [result] = await pool.query(
+      "INSERT INTO vota (id_obra, id_usuario, puntaje) VALUES (?, ?, ?)",
+      [id_obra, id_usuario, puntaje]
+    );
+
+    res.json({
+      id_obra,
+      id_usuario,
+      puntaje,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Actualizar un voto
+export const updateVoto = async (req, res) => {
+  try {
+    const { puntaje } = req.body;
+    const [result] = await pool.query(
+      "UPDATE vota SET puntaje = ? WHERE id_obra = ? AND id_usuario = ?",
+      [puntaje, req.params.id_obra, req.params.id_usuario]
+    );
+
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Voto not found" });
+
+    const [updatedVoto] = await pool.query(
+      "SELECT * FROM vota WHERE id_obra = ? AND id_usuario = ?",
+      [req.params.id_obra, req.params.id_usuario]
+    );
+
+    res.json(updatedVoto[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Eliminar un voto
+export const deleteVoto = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "DELETE FROM vota WHERE id_obra = ? AND id_usuario = ?",
+      [req.params.id_obra, req.params.id_usuario]
+    );
+
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Voto not found" });
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import eventosRoutes from "./routes/eventos.routes.js";
 import obrasRoutes from "./routes/obras.routes.js";
 import escultoresRoutes from "./routes/escultores.routes.js";
 import usuariosRoutes from "./routes/usuarios.routes.js";
+import votaRoutes from "./routes/vota.routes.js";
 import imagenesRoutes from "./routes/imagenes.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
@@ -29,5 +30,7 @@ app.use(indexRoutes);
 app.use("/obras", obrasRoutes);
 app.use("/imagenes", imagenesRoutes);
 app.use("/usuarios", usuariosRoutes);
+
+app.use("/vota", votaRoutes);
 
 app.listen(PORT);
