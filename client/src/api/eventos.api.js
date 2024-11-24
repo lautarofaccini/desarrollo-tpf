58d2a61c754fe03604f0a2a733fb96f001ba0bbe
Lautaro Faccini <lautarofaccini@gmail.com>
2024-11-24 10:40:57 -0300

    Controles de evento implementados en /eventos/:id y estado de votacion en /eventos Base de datos modificada para adaptar los cambios

--- client/src/api/eventos.api.js
+++ client/src/api/eventos.api.js
@@ -15,3 +15,15 @@ export const getEventoRequest = async (id) => await axios.get(`/eventos/${id}`);
 
 export const updateEventoRequest = async (id, newFields) =>
   await axios.put(`/eventos/${id}`, newFields);
+
+export const activarEventoRequest = async (id) =>
+  await axios.post(`/eventos/activar/${id}`);
+
+export const pausarEventoRequest = async (id) =>
+  await axios.post(`/eventos/pausar/${id}`);
+
+export const finalizarEventoRequest = async (id) =>
+  await axios.post(`/eventos/finalizar/${id}`);
+
+export const desactivarEventoRequest = async (id) =>
+  await axios.post(`/eventos/desactivar/${id}`);
