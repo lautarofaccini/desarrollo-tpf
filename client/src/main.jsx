7d992b7f38d4847b59a34264e3202901ffc3e187
Lautaro Faccini <lautarofaccini@gmail.com>
2024-12-02 13:19:42 -0300

    NextUI Setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthProvider";
+import { NextUIProvider } from "@nextui-org/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
       <BrowserRouter>
-        <App />
+        <NextUIProvider>
+          <main className="dark text-foreground bg-background">
+            <App />
+          </main>
+        </NextUIProvider>
       </BrowserRouter>
     </AuthContextProvider>
   </StrictMode>
