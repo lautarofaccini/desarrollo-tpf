0578d5486e17f3580ff9dd07eccd00780d06c7ce
Lautaro Faccini <lautarofaccini@gmail.com>
2024-12-02 11:18:02 -0300

    Map component

---
+++ client/src/components/GoogleMapComponent.jsx
@@ -0,0 +1,38 @@
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  AdvancedMarkerAnchorPoint,
+} from "@vis.gl/react-google-maps";
+
+const GoogleMapComponent = () => {
+  const mapContainerStyle = {
+    width: "100%",
+    height: "400px",
+    borderRadius: "16px",
+  };
+
+  const center = {
+    lat: -27.4372388, // Coordenadas de la Bienal del Chaco
+    lng: -58.9813493,
+  };
+
+  return (
+    <APIProvider apiKey={"AIzaSyDmLdubB5_uwmekbVU4EXzMVJUlJuEklgs"}>
+      <Map
+        style={mapContainerStyle}
+        defaultCenter={center}
+        defaultZoom={16}
+        gestureHandling={"greedy"}
+        mapId="DEMO_MAP_ID"
+      >
+        <AdvancedMarker
+          position={center}
+          anchorPoint={AdvancedMarkerAnchorPoint.TOP_LEFT}
+        />
+      </Map>
+    </APIProvider>
+  );
+};
+
+export default GoogleMapComponent;
