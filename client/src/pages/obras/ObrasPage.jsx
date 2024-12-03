800842c3ec42a83892ff93d3b377a31d9a251f29
Mauri <95150798+MauriMS@users.noreply.github.com>
2024-12-03 17:57:05 -0300

    Permitir el scroll en Obras

--- client/src/pages/obras/ObrasPage.jsx
+++ client/src/pages/obras/ObrasPage.jsx
@@ -20,7 +20,7 @@ function ObrasPage() {
   };
 
   return (
-    <div className="bg-gradient-to-br from-pink-300 via-purple-500 to-indigo-300 text-white flex flex-col justify-between px-20 py-4">
+    <div className="bg-gradient-to-br from-pink-300 via-purple-500 to-indigo-300 text-white flex flex-col justify-between px-20 py-4 container-scrollable pb-20" style={{ maxHeight: 'calc(100vh - 4rem)'}} >
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold mb-8 hc-blue text-white text-center">
           Obras
