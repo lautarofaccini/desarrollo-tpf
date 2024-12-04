ad60ef425ca079f4838445489ddcf15f07eb82ef
Juani <gez.schegtel_ji@proton.me>
2024-12-04 12:18:18 -0300

    Script para creación de la base de datos con datos predeterminados

---
+++ database/Creación de base de datos/desarrollo_tpf_eventos.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: desarrollo_tpf
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `eventos`
+--
+
+LOCK TABLES `eventos` WRITE;
+/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
+INSERT INTO `eventos` VALUES (1,'2023-11-14 16:00:00','2023-12-16 00:00:00','Museo de Arte Moderno','Exposición de esculturas abstractas','Abstracto','finalizado'),(2,'2024-11-01 12:00:00','2024-12-20 20:00:00','Galería Nacional','Esculturas de temática histórica','Historia','activo'),(3,'2026-03-10 14:00:00','2026-06-10 20:00:00','Centro Cultural','Esculturas contemporáneas de artistas jóvenes','Contemporáneo','inactivo');
+/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-01 23:10:52
---
+++ database/Creación de base de datos/desarrollo_tpf_imagenes.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: desarrollo_tpf
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `imagenes`
+--
+
+LOCK TABLES `imagenes` WRITE;
+/*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
+INSERT INTO `imagenes` VALUES (1,'https://storage.googleapis.com/obrasbienal/1_1731612765093.webp',1),(2,'https://storage.googleapis.com/obrasbienal/1_1731612765862.webp',1),(3,'https://storage.googleapis.com/obrasbienal/1_1731612766105.webp',1),(4,'https://storage.googleapis.com/obrasbienal/2_1731614437443.webp',2),(5,'https://storage.googleapis.com/obrasbienal/2_1731614437902.webp',2),(6,'https://storage.googleapis.com/obrasbienal/2_1731614438133.webp',2),(7,'https://storage.googleapis.com/obrasbienal/3_1731614456833.webp',3),(8,'https://storage.googleapis.com/obrasbienal/3_1731614457324.webp',3),(9,'https://storage.googleapis.com/obrasbienal/3_1731614457580.webp',3),(10,'https://storage.googleapis.com/obrasbienal/4_1731614466867.webp',4),(11,'https://storage.googleapis.com/obrasbienal/4_1731614467362.webp',4),(12,'https://storage.googleapis.com/obrasbienal/4_1731614467600.webp',4),(13,'https://storage.googleapis.com/obrasbienal/5_1731614480235.webp',5),(14,'https://storage.googleapis.com/obrasbienal/5_1731614480737.webp',5),(15,'https://storage.googleapis.com/obrasbienal/5_1731614480998.webp',5),(16,'https://storage.googleapis.com/obrasbienal/6_1731614518488.webp',6),(17,'https://storage.googleapis.com/obrasbienal/6_1731614518818.webp',6),(18,'https://storage.googleapis.com/obrasbienal/6_1731614519073.webp',6),(19,'https://storage.googleapis.com/obrasbienal/7_1731614531213.webp',7),(20,'https://storage.googleapis.com/obrasbienal/7_1731614531684.webp',7),(21,'https://storage.googleapis.com/obrasbienal/7_1731614531926.webp',7),(22,'https://storage.googleapis.com/obrasbienal/8_1731614554335.webp',8),(23,'https://storage.googleapis.com/obrasbienal/8_1731614554781.webp',8),(24,'https://storage.googleapis.com/obrasbienal/8_1731614555027.webp',8),(25,'https://storage.googleapis.com/obrasbienal/9_1731614563838.webp',9),(26,'https://storage.googleapis.com/obrasbienal/9_1731614564305.webp',9),(27,'https://storage.googleapis.com/obrasbienal/9_1731614564562.webp',9);
+/*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-01 23:10:52
