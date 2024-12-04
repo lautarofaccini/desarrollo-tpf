08f6abe6b1339a98f66ab35dccdfb07b618b5092
Juani <gez.schegtel_ji@proton.me>
2024-12-04 12:18:18 -0300

    Script para creación de la base de datos con datos predeterminados

---
+++ database/Creación de base de datos/desarrollo_tpf_obras.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: desarrollo_tpf
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `obras`
+--
+
+LOCK TABLES `obras` WRITE;
+/*!40000 ALTER TABLE `obras` DISABLE KEYS */;
+INSERT INTO `obras` VALUES (1,'2023-01-10','Escultura abstracta de metal','Metal','Abstracto',0.00,1,1),(2,'2022-05-15','Figura histórica en mármol','Mármol','Histórico',0.00,2,2),(3,'2023-11-03','Escultura de madera inspirada en la naturaleza','Madera','Naturalista',0.00,3,3),(4,'2024-04-12','Escultura abstracta en vidrio','Vidrio','Abstracto',0.00,1,2),(5,'2023-07-08','Figura histórica en bronce','Bronce','Histórico',0.00,2,3),(6,'2023-12-01','Escultura moderna de hierro','Hierro','Moderno',0.00,3,1),(7,'2025-10-30','Figura geométrica de mármol','Mármol','Geométrico',0.00,1,3),(8,'2025-11-05','Escultura naturalista en madera','Madera','Naturalista',0.00,2,1),(9,'2024-01-19','Escultura abstracta de arcilla','Arcilla','Abstracto',0.00,3,2);
+/*!40000 ALTER TABLE `obras` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-01 23:10:52
