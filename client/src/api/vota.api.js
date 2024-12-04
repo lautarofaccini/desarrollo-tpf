82bede63a960cabaf1abb20b3d69bdd93b7de97d
Lautaro Faccini <lautarofaccini@gmail.com>
2024-12-03 23:53:13 -0300

    Evento no se puede votar si ya votaste

--- client/src/api/vota.api.js
+++ client/src/api/vota.api.js
@@ -2,11 +2,26 @@ import axios from "./axios";
 
 export const getVotoRequest = async () => await axios.get(`/vota`);
 
+export const getVotoByObraUsuarioRequest = async (idObra, idUsuario) => {
+  try {
+    const response = await axios.get(
+      `/vota?idObra=${idObra}&idUsu=${idUsuario}`
+    );
+    return response.data; // Si encuentra un voto, lo devuelve
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      // Si el error es 404, significa que no hay voto
+      return null; // Devolver null para manejar este caso como una condición normal
+    }
+    throw error; // Lanza otros errores que no sean 404
+  }
+};
+
 export const getVotoByObraRequest = async (id) =>
   await axios.get(`/vota/obra/${id}`);
 
 export const getVotoByUsuarioRequest = async (id) =>
-    await axios.get(`/vota/usuario/${id}`);
+  await axios.get(`/vota/usuario/${id}`);
 
 export const createVotoRequest = async (voto) =>
   await axios.post(`/vota`, voto);
