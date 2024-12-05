567aaa50337f8db00f30a5f1d87e9c205a063060
Lautaro Faccini <lautarofaccini@gmail.com>
2024-12-05 10:13:56 -0300

    Solucionado el problema de los .env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { VITE_IP } from "../../../server/config";
 
+// Usa import.meta.env para las variables de entorno en Vite
 const instance = axios.create({
-  baseURL: `http://${VITE_IP}:4000/api`,
+  baseURL: `http://${import.meta.env.VITE_HOST || "localhost"}:${
+    import.meta.env.VITE_PORT || 4000
+  }/api`,
   withCredentials: true,
 });
 
