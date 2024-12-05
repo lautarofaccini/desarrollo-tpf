626d96ef6771baa52ad72d10647bd97d69e8b39c
Lautaro Faccini <lautarofaccini@gmail.com>
2024-12-05 10:33:57 -0300

    Error mio!

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Usa import.meta.env para las variables de entorno en Vite
 const instance = axios.create({
   baseURL: `http://${import.meta.env.VITE_HOST || "localhost"}:${
-    import.meta.env.VITE_PORT || 4000
+    import.meta.env.VITE_SERVER_PORT || 4000
   }/api`,
   withCredentials: true,
 });
